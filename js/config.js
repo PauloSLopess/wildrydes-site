9ba5a26aaf5887726941a3b6675df43c5e9f60f2
Paulo Sousa Lopes <144073082+PauloSLopess@users.noreply.github.com>
2026-02-07 10:50:18 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,12 +2,12 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_67e8IkMsW', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2gmuer639k44tcm8m2b92shdqb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        client_secret = 'eosupqglg69eocvgfng494scdv30fd6k8n18p6ihtkcvc26o6rf',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
