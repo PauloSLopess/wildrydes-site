37328a6541aac1448a77dbdd19998c4b1220d79b
Paulo Sousa Lopes <144073082+PauloSLopess@users.noreply.github.com>
2026-02-07 10:56:49 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_67e8IkMsW', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2gmuer639k44tcm8m2b92shdqb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_25i05F7PD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3fp5hvl2b9n304vkigs537s6sv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
